cc99d4b0f49cf7f54f175044421776e53d7b719d
AugustoConti <augustoconti10@gmail.com>
2018-11-16 20:04:59 -0300

    Completo test ubicaciones individuales

--- src/test/java/epers/bichomon/service/feed/FeedUbicacionServiceTest.java
+++ src/test/java/epers/bichomon/service/feed/FeedUbicacionServiceTest.java
@@ -1,18 +1,24 @@
 package epers.bichomon.service.feed;
 
 import epers.bichomon.AbstractConectadosTest;
+import epers.bichomon.model.bicho.Bicho;
 import epers.bichomon.model.especie.Especie;
 import epers.bichomon.model.especie.TipoBicho;
+import epers.bichomon.model.evento.*;
+import epers.bichomon.model.ubicacion.Dojo;
+import epers.bichomon.model.ubicacion.Guarderia;
 import epers.bichomon.model.ubicacion.Pueblo;
 import epers.bichomon.model.ubicacion.Ubicacion;
 import epers.bichomon.service.ServiceFactory;
 import epers.bichomon.service.bicho.BichoService;
 import epers.bichomon.service.mapa.MapaService;
+import jersey.repackaged.com.google.common.collect.Sets;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FeedUbicacionServiceTest extends AbstractConectadosTest {
 
@@ -28,105 +34,116 @@ public class FeedUbicacionServiceTest extends AbstractConectadosTest {
 
     @Test
     void ubicacionInexistenteRetornaListaVacia() {
-        assertThrows(EntrenadorInexistenteException.class, () -> feedService.feedUbicacion(""));
+        assertThrows(EntrenadorInexistenteException.class, () -> feedService.feedUbicacion("fruta"));
     }
 
     @Test
     void ubicacionNuevaSinEventosRetornaListaVacia() {
         mapService.crearUbicacion(new Pueblo("Pueblucho"));
         newEntrenador("nuevo", testService.getByName(Ubicacion.class, "Pueblucho"));
-        assertTrue(feedService.feedEntrenador("nuevo").isEmpty());
+        assertTrue(feedService.feedUbicacion("nuevo").isEmpty());
     }
 
-//    @Test
-//    void EntrenadorViajoFeedUbicacionMuestraElViaje() {
-//        String e = "entrenador";
-//        newEntrenador(e, testService.getByName(Ubicacion.class, "Plantalandia"));
-//
-//        mapService.mover(e, "Agualandia");
-//
-//        List<Evento> eventos = feedService.feedEntrenador(e);
-//        assertEquals(1, eventos.size());
-//        checkEvento(eventos.get(0), e, "Agualandia", TipoEvento.Arribo);
-//    }
-//
-//    @Test
-//    void EntrenadorViajo2VecesMuestraViajesEnOrden() {
-//        String e = "viajador2";
-//        newEntrenador(e, testService.getByName(Ubicacion.class, "Plantalandia"));
-//
-//        mapService.mover(e, "Agualandia");
-//        mapService.mover(e, "Plantalandia");
-//
-//        List<Evento> eventos = feedService.feedEntrenador(e);
-//        assertEquals(2, eventos.size());
-//        checkEvento(eventos.get(0), e, "Plantalandia", TipoEvento.Arribo);
-//        checkEvento(eventos.get(1), e, "Agualandia", TipoEvento.Arribo);
-//    }
-//
-//    @Test
-//    void FeedUbicacionConEventoCaptura() {
-//        Bicho bichin = testService.getByName(Especie.class, "rocamon").crearBicho();
-//        testService.save(bichin);
-//        String trainer = "trainer";
-//        Ubicacion place = new Guarderia("guardaBicho");
-//        testService.save(place);
-//        place.abandonar(bichin);
-//        testService.upd(place);
-//        newEntrenador(trainer, place);
-//
-//        bichoService.buscar(trainer);
-//
-//        List<Evento> eventos = feedService.feedEntrenador(trainer);
-//        assertEquals(1, eventos.size());
-//        checkEvento(eventos.get(0), trainer, "guardaBicho", TipoEvento.Captura);
-//    }
-//
-//    @Test
-//    void FeedUbicacionConEventoAbandono() {
-//        Especie e = testService.getByName(Especie.class, "rocamon");
-//        Bicho b1 = e.crearBicho();
-//        Bicho b2 = e.crearBicho();
-//
-//        newEntrenador("lucas", testService.getByName(Guarderia.class, "Poke"), Sets.newHashSet(b1, b2));
-//
-//        bichoService.abandonar("lucas", b1.getID());
-//
-//        List<Evento> eventos = feedService.feedEntrenador("lucas");
-//        assertEquals(1, eventos.size());
-//        checkEvento(eventos.get(0), "lucas", "Poke", TipoEvento.Abandono);
-//    }
-//
-//    @Test
-//    void FeedUbicacionConEventoCoronado() {
-//        Bicho b = testService.getByName(Especie.class, "rocamon").crearBicho();
-//        newEntrenador("brock", testService.getByName(Dojo.class, "Tibet Dojo"), Sets.newHashSet(b));
-//
-//        bichoService.duelo("brock", b.getID());
-//
-//        List<Evento> eventos = feedService.feedEntrenador("brock");
-//        assertEquals(1, eventos.size());
-//        checkEvento(eventos.get(0), "brock", "Tibet Dojo", TipoEvento.Coronacion);
-//    }
-//
-//    @Test
-//    void FeedUbicacionConEventoCoronadoYDescoronado() {
-//        String dojo = "A1";
-//        Bicho b1 = testService.getByName(Especie.class, "rocamon").crearBicho();
-//        newEntrenador("alberto", testService.getByName(Dojo.class, dojo), Sets.newHashSet(b1));
-//        Bicho b2 = testService.getByName(Especie.class, "metalmon").crearBicho();
-//        newEntrenador("julio", testService.getByName(Dojo.class, dojo), Sets.newHashSet(b2));
-//
-//        bichoService.duelo("alberto", b1.getID());
-//        bichoService.duelo("julio", b2.getID());
-//
-//        List<Evento> eventosAlberto = feedService.feedEntrenador("alberto");
-//        assertEquals(2, eventosAlberto.size());
-//        checkEvento(eventosAlberto.get(0), "alberto", dojo, TipoEvento.Coronacion);
-//        checkEvento(eventosAlberto.get(1), "alberto", dojo, TipoEvento.Coronacion);
-//
-//        List<Evento> eventosJulio = feedService.feedEntrenador("julio");
-//        assertEquals(1, eventosJulio.size());
-//        checkEvento(eventosJulio.get(0), "julio", dojo, TipoEvento.Coronacion);
-//    }
+    @Test
+    void EntrenadorViajoFeedUbicacionMuestraElViaje() {
+        String e = "entrenador";
+        mapService.crearUbicacion(new Pueblo("p1"));
+        mapService.crearUbicacion(new Pueblo("p2"));
+        mapService.conectar("p1", "p2", "Terrestre");
+        newEntrenador(e, testService.getByName(Ubicacion.class, "p1"));
+
+        mapService.mover(e, "p2");
+
+        List<Evento> eventos = feedService.feedUbicacion(e);
+        assertEquals(1, eventos.size());
+        checkEvento((EventoArribo) eventos.get(0), e, "p1", "p2");
+    }
+
+    @Test
+    void EntrenadorViajo2VecesMuestraViajesEnOrden() {
+        String e = "viajador2";
+        mapService.crearUbicacion(new Pueblo("p3"));
+        mapService.crearUbicacion(new Pueblo("p4"));
+        mapService.conectar("p3", "p4", "Terrestre");
+        mapService.conectar("p4", "p3", "Terrestre");
+        newEntrenador(e, testService.getByName(Ubicacion.class, "p3"));
+
+        mapService.mover(e, "p4");
+        mapService.mover(e, "p3");
+
+        List<Evento> eventos = feedService.feedUbicacion(e);
+        assertEquals(2, eventos.size());
+        checkEvento((EventoArribo) eventos.get(0), e, "p4", "p3");
+        checkEvento((EventoArribo) eventos.get(1), e, "p3", "p4");
+    }
+
+    @Test
+    void FeedUbicacionConEventoCaptura() {
+        Bicho bichin = testService.getByName(Especie.class, "rocamon").crearBicho();
+        testService.save(bichin);
+        String trainer = "trainer";
+        Ubicacion place = new Guarderia("guardaBicho");
+        testService.save(place);
+        place.abandonar(bichin);
+        testService.upd(place);
+        newEntrenador(trainer, place);
+
+        bichoService.buscar(trainer);
+
+        List<Evento> eventos = feedService.feedUbicacion(trainer);
+        assertEquals(1, eventos.size());
+        checkEvento((EventoCaptura) eventos.get(0), trainer, "guardaBicho", "rocamon");
+    }
+
+    @Test
+    void FeedUbicacionConEventoAbandono() {
+        mapService.crearUbicacion(new Guarderia("p5"));
+
+        Especie e = testService.getByName(Especie.class, "rocamon");
+        Bicho b1 = e.crearBicho();
+        Bicho b2 = e.crearBicho();
+
+        newEntrenador("lucas", testService.getByName(Guarderia.class, "p5"), Sets.newHashSet(b1, b2));
+
+        bichoService.abandonar("lucas", b1.getID());
+
+        List<Evento> eventos = feedService.feedUbicacion("lucas");
+        assertEquals(1, eventos.size());
+        checkEvento((EventoAbandono) eventos.get(0), "lucas", "p5", "rocamon");
+    }
+
+    @Test
+    void FeedUbicacionConEventoCoronado() {
+        mapService.crearUbicacion(new Dojo("p6"));
+        Bicho b = testService.getByName(Especie.class, "rocamon").crearBicho();
+        newEntrenador("brock", testService.getByName(Dojo.class, "p6"), Sets.newHashSet(b));
+
+        bichoService.duelo("brock", b.getID());
+
+        List<Evento> eventos = feedService.feedUbicacion("brock");
+        assertEquals(1, eventos.size());
+        checkEvento((EventoCoronacion) eventos.get(0), "brock", "", "p6");
+    }
+
+    @Test
+    void FeedUbicacionConEventoCoronadoYDescoronado() {
+        mapService.crearUbicacion(new Dojo("p7"));
+        Bicho b1 = testService.getByName(Especie.class, "rocamon").crearBicho();
+        newEntrenador("alberto", testService.getByName(Dojo.class, "p7"), Sets.newHashSet(b1));
+        Bicho b2 = testService.getByName(Especie.class, "metalmon").crearBicho();
+        newEntrenador("julio", testService.getByName(Dojo.class, "p7"), Sets.newHashSet(b2));
+
+        bichoService.duelo("alberto", b1.getID());
+        bichoService.duelo("julio", b2.getID());
+
+        List<Evento> eventosAlberto = feedService.feedUbicacion("alberto");
+        assertEquals(2, eventosAlberto.size());
+        checkEvento((EventoCoronacion) eventosAlberto.get(0), "julio", "alberto", "p7");
+        checkEvento((EventoCoronacion) eventosAlberto.get(1), "alberto", "", "p7");
+
+        List<Evento> eventosJulio = feedService.feedUbicacion("julio");
+        assertEquals(2, eventosJulio.size());
+        checkEvento((EventoCoronacion) eventosJulio.get(0), "julio", "alberto", "p7");
+        checkEvento((EventoCoronacion) eventosAlberto.get(1), "alberto", "", "p7");
+    }
 }
